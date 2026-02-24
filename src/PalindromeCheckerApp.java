3f012df7c9f4015f7c48a16ccfee40f990e4fe34
shamal2005 <mohdshamal13@gmail.com>
2026-02-24 16:16:43 +0530

    UC3

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,26 +1,27 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args){
 
-                // Hardcoded string (String Literal)
-                String word = "madam";
+
+
+                // Original string
+                String original = "level";
 
                 // Variable to store reversed string
                 String reversed = "";
 
-                // Reverse the string using loop
-                for (int i = word.length() - 1; i >= 0; i--) {
-                    reversed += word.charAt(i);
+                // Reverse the string using for loop
+                for (int i = original.length() - 1; i >= 0; i--) {
+                    reversed = reversed + original.charAt(i);
                 }
 
-                // Check palindrome using if-else
-                if (word.equals(reversed)) {
-                    System.out.println("The given string \"" + word + "\" is a Palindrome.");
+                // Compare original and reversed using equals()
+                if (original.equals(reversed)) {
+                    System.out.println("The given string \"" + original + "\" is a Palindrome.");
                 } else {
-                    System.out.println("The given string \"" + word + "\" is NOT a Palindrome.");
+                    System.out.println("The given string \"" + original + "\" is NOT a Palindrome.");
                 }
-
-                // Program exits automatically after execution
             }
         }
+
 
 
