ba052d62b3ae1c76b002d564be1f4660a0a9a4e3
shamal2005 <mohdshamal13@gmail.com>
2026-02-24 16:27:49 +0530

    UC4 executed

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,26 +2,36 @@ public class PalindromeCheckerApp {
     public static void main(String[] args){
 
 
-
                 // Original string
-                String original = "level";
+                String original = "radar";
 
-                // Variable to store reversed string
-                String reversed = "";
+                // Convert string to character array
+                char[] characters = original.toCharArray();
 
-                // Reverse the string using for loop
-                for (int i = original.length() - 1; i >= 0; i--) {
-                    reversed = reversed + original.charAt(i);
+                // Two-pointer approach
+                int start = 0;
+                int end = characters.length - 1;
+
+                boolean isPalindrome = true;
+
+                while (start < end) {
+                    if (characters[start] != characters[end]) {
+                        isPalindrome = false;
+                        break;
+                    }
+                    start++;
+                    end--;
                 }
 
-                // Compare original and reversed using equals()
-                if (original.equals(reversed)) {
+                // Display result
+                if (isPalindrome) {
                     System.out.println("The given string \"" + original + "\" is a Palindrome.");
                 } else {
                     System.out.println("The given string \"" + original + "\" is NOT a Palindrome.");
                 }
             }
         }
+
 
 
 
